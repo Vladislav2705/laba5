a294abcbf5acdaa4e3465946a7c5f57762f4c310
Vladislav2705 <44916798+Vladislav2705@users.noreply.github.com>
2019-01-14 01:33:05 +0600

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Array abc = new Array();
+        abc.createArray();
+        System.out.println(abc.strList.get(5));
+        Vlad abcd = new Vlad();
+        abcd.mapping();
+        abcd.out(10);
+        abcd.outall();
+	// write your code here
+    }
+}
+
+
